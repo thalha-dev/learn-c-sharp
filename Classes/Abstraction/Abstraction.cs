23d43ea834215f5dde43436042be1a71fc34f8f1
thalha-dev <dev.thalha@gmail.com>
2024-01-20 16:37:16 +0530

    Abstract Class

---
+++ Classes/Abstraction/Abstraction.cs
@@ -0,0 +1,26 @@
+namespace Abstraction
+{
+    // Abstract class
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using MethodOverriding;
+﻿using Abstraction;
 
 namespace core_1
 {
@@ -6,13 +6,10 @@ namespace core_1
     {
         static void Main(string[] args)
         {
-            Animal myAnimal = new Animal();  // Create a Animal object
             Animal myPig = new Pig();  // Create a Pig object
-            Animal myDog = new Dog();  // Create a Dog object
 
-            myAnimal.animalSound();
             myPig.animalSound();
-            myDog.animalSound();
+            myPig.sleep();
         }
     }
 }
